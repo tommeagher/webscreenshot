da378f44bf0011475e5de810a34dc38e90ac35dc
Tom Meagher <hello+git@tommeagher.com>
2020-11-24 11:58:29 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Webscreenshot',
+    version='v2.94',
+    packages=['webscreenshot',],
+    license='GNU General Public License',
+    long_description=open('README.txt').read(),
+)
